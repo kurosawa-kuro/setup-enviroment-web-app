a7fb83dc9ba748fb39dc3ea274d5495ff00acb13
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-19 11:24:50 +0900

    Update setup-lib-amazon-linux-2023 -user-data.sh

--- script/setup-lib-amazon-linux-2023 -user-data.sh
+++ script/setup-lib-amazon-linux-2023 -user-data.sh
@@ -52,6 +52,26 @@ usermod -a -G docker ec2-user
 # nodejsのインストール
 sudo dnf install -y nodejs npm
 
+# Go言語のインストール
+echo "Installing Go language..."
+GO_VERSION="1.22.0"
+wget "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+rm -rf /usr/local/go
+tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+rm "go${GO_VERSION}.linux-amd64.tar.gz"
+
+# PATH設定
+if ! grep -q "/usr/local/go/bin" /home/ec2-user/.bashrc; then
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/ec2-user/.bashrc
+    echo 'export GOPATH=$HOME/go' >> /home/ec2-user/.bashrc
+    echo 'export PATH=$PATH:$GOPATH/bin' >> /home/ec2-user/.bashrc
+fi
+
+# GOROOTとGOPATHの設定
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
 # バージョン確認
 echo "Checking installed versions..."
 echo "System packages updated: $(date)"
@@ -62,6 +82,7 @@ echo "Ansible version: $(ansible --version | head -n1)"
 echo "Docker version: $(docker --version)"
 echo "Docker Compose version: $(docker-compose --version)"
 echo "Node version: $(node -v)"
+echo "Go version: $(go version)"
 
 # 完了メッセージ
 echo "End: User data script execution - $(date)"
