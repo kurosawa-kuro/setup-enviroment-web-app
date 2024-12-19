2f86edac89e2cfcb34488b3b7f85494fd57bdf8e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-12-19 10:50:07 +0900

    Update setup-lib-amazon-linux-2023 -user-data.sh

--- script/setup-lib-amazon-linux-2023 -user-data.sh
+++ script/setup-lib-amazon-linux-2023 -user-data.sh
@@ -3,9 +3,6 @@
 # エラー発生時にスクリプトを停止
 set -e
 
-# ログファイルの設定
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-
 echo "Begin: User data script execution - $(date)"
 
 # システムアップデート
